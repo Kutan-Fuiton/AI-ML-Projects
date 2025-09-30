95645c31fbc6d846f7fb6d0c09b2d1c90666219f
Kutan-Fuiton <subarnochakraborty123@gmail.com>
2025-09-30 10:38:39 +0530

    commit changes

--- Handwritten Digit Recognizer/model/run_model.py
+++ Handwritten Digit Recognizer/model/run_model.py
@@ -1,8 +1,13 @@
+# run_model just run the model on the test dataset 
+
 import keras
+import matplotlib
+matplotlib.use('Agg') 
 from keras.datasets import mnist
 from keras.models import load_model
 from keras.utils import to_categorical
 import numpy as np
+import matplotlib.pyplot as plt
 
 # Load trained model
 model = load_model("model/mnist_cnn.h5")
@@ -17,14 +22,20 @@ y_test = to_categorical(y_test, 10)
 
 # Evaluate model performance
 loss, acc = model.evaluate(x_test, y_test, verbose=0)
-print(f"📊 Test Accuracy: {acc * 100:.2f}%")
+print(f"Test Accuracy: {acc * 100:.2f}%")
 
 # Pick a sample image
-sample_index = 0  
+sample_index = 12  
 sample_image = x_test[sample_index].reshape(1, 28, 28, 1)
 
 # Predict
 prediction = model.predict(sample_image)
 predicted_digit = np.argmax(prediction)
-
 print(f"🔢 Predicted Digit: {predicted_digit}")
+
+plt.imshow(x_test[sample_index].reshape(28, 28), cmap="gray")
+plt.title(f"Prediction: {predicted_digit}")
+plt.axis("off")
+# Save the visualization to a file
+plt.savefig("predicted_digit.png")
+print("Image saved as predicted_digit.png")
--- Handwritten Digit Recognizer/model/testing_model.py
+++ Handwritten Digit Recognizer/model/testing_model.py
@@ -20,10 +20,10 @@ y_test = to_categorical(y_test, 10)
 
 # Evaluate model performance
 loss, acc = model.evaluate(x_test, y_test, verbose=0)
-print(f"📊 Test Accuracy: {acc * 100:.2f}%")
+print(f"Test Accuracy: {acc * 100:.2f}%")
 
 # Load image
-img = Image.open("model/image1.png").convert("L")
+img = Image.open("model/images/image2.png").convert("L")
 img = img.resize((28, 28))
 img_array = np.array(img).astype("float32") / 255.0
 img_array = img_array.reshape(1, 28, 28, 1)
@@ -33,9 +33,8 @@ prediction = model.predict(img_array)
 predicted_digit = np.argmax(prediction)
 print(f"🔢 Predicted Digit: {predicted_digit}")
 
-# Show the image (before feeding into model)
 plt.imshow(img_array.reshape(28, 28), cmap="gray")
 plt.title(f"Prediction: {predicted_digit}")
 plt.axis("off")
 plt.savefig("predicted_digit.png")  # saves to file
-print("✅ Image saved as predicted_digit.png")
+print("Image saved as predicted_digit.png")
